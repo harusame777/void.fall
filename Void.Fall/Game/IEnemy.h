e4a814b79497ec7f86b61e4fa49e47adb6fa8422
takeda haruse <ca01234015@st.kawahara.ac.jp>
2023-10-30 11:33:24 +0900

    void.fall 2023/10/30 player+
    回避モデル、アニメーションを追加

--- Void.Fall/Game/IEnemy.h
+++ Void.Fall/Game/IEnemy.h
@@ -1,17 +1,25 @@
 #pragma once
+#include "Game.h"
 #include "P_main_Player.h"
 #include "collision/CollisionObject.h"
+class Game;
 class P_main_Player;
 class IEnemy : public IGameObject
 {
 public:
-	IEnemy() {};
+	IEnemy() {
+		//enemyvec[m_game->m_numenemy] = &m_position;
+	};
+	//void Vecenemynew(){
+		//enemyvec[m_game->m_numenemy] = new Vector3;
+	//}
 	virtual ~IEnemy(){};
 public:
 	P_main_Player* m_player = nullptr;		//プレイヤー
 	CharacterController m_charaCon ;        //キャラコン
 	CollisionObject* m_collisionObject;		//コリジョンオブジェクト。
 	Vector3 m_position;						//座標
+	Game* m_game;
 
 };
 
